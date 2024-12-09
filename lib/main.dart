8a0df926ab17ba18bfe0518fc1055f6ee0e54659
Aseem Akbar <aseemkv66@gmail.com>
2024-12-09 10:23:08 +0530

    new updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:makemeover/firebase_options.dart';
 import 'package:makemeover/view/LoginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:makemeover/view/artistProfile.dart';
 
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -17,8 +17,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Loginpage(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Loginpage(),
+        routes: {
+          // '/Stephy': (context) => const MyWidget(name),
+        });
   }
 }
