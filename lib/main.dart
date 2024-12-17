15b19d25724b5fb4948bd402cae5f4d2f86ac001
unknown <heyshebiba@gmail.com>
2024-12-17 11:57:32 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: Loginpage(),
         routes: {
           // '/Stephy': (context) => const MyWidget(name),
         });
