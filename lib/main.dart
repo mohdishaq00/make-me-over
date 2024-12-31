54422949f58d41f6f8e45122c38e82ea3983b564
mohdishaq00 <ishaqkalathingal@gmail.com>
2024-12-31 21:00:21 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:makemeover/bookingpage.dart';
 
-// import 'package:makemeover/view/artistCard.dart';
-import 'package:makemeover/view/home.dart';
+// import 'package:makemeover/view/home.dart';
 
 import 'package:makemeover/model/firebase_options.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:makemeover/view/LoginPage.dart';
-
-//  import 'package:makemeover/view/home.dart';
-// import 'package:makemeover/view/artistProfile.dart';
-// import 'package:makemeover/view/home.dart';
-// import 'package:makemeover/view/home.dart';
-// import 'package:makemeover/view/overview.dart';
+import 'package:makemeover/view/LoginPage.dart';
+import 'package:makemeover/view/home.dart';
+import 'package:makemeover/view/profile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => WishlistProvider()),
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -33,7 +23,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage () );
-  
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
+
+
+
+
+
+// class WishlistProvider extends ChangeNotifier {
+//   final List<Map<String, String>> _Wishlist = [];
+
+//   List<Map<String, String>> get Wishlist => _Wishlist;
+
+//   void addToWishlist(Map<String, String> item) {
+//     _Wishlist.add(item);
+//     notifyListeners();
+//   }
+//   bool isInWishlist(Map<String, String> item) {
+//     return _Wishlist.contains(item);
+//   }
+// }
+
